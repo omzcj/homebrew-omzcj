6f8ec53c4a079f28a51138854a7128c3266e0aee
朝杰 张 <7605023+assassinor@users.noreply.github.com>
2021-05-24 00:37:09 +0800

    commit

--- Formula/omzcj.rb
+++ Formula/omzcj.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Omzcj < Formula
   desc "omzcj"
   homepage "https://assassinor.github.io"
@@ -8,14 +5,7 @@ class Omzcj < Formula
   version "0.0.1"
   license ""
 
-  # depends_on "cmake" => :build
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     bin.install "bin/app2ipa"
     bin.install "bin/cartool"
     bin.install "bin/dotfiles"
@@ -27,15 +17,6 @@ class Omzcj < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test trial-reset`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end

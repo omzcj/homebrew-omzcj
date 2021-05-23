42b8024a22a3af76cc34e89ea1b4eed1e10d4929
朝杰 张 <zhangchaojie@bytedance.com>
2021-05-24 00:27:52 +0800

    commit

---
+++ Formula/omzcj.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Omzcj < Formula
+  desc "omzcj"
+  homepage "https://assassinor.github.io"
+  url "https://github.com/assassinor/opt/archive/refs/tags/0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "1a08bf47d07306574b841cd6d432257fe0d47b34bf14150d038d24ea1fc53b50"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    # bin.install "bin/*"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test trial-reset`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end

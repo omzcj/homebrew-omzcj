eca30b787e08dab737b58556e7a38150b479c87c
朝杰 张 <zhangchaojie@bytedance.com>
2021-05-24 01:11:07 +0800

    commit

---
+++ omzcj.rb
@@ -0,0 +1,23 @@
+class Omzcj < Formula
+  desc "omzcj"
+  homepage "https://assassinor.github.io"
+  url "https://github.com/assassinor/omzcj.git"
+  version "0.0.1"
+  license ""
+
+  def install
+    bin.install Dir["bin/*"]
+    # bin.install "bin/app2ipa"
+    # bin.install "bin/cartool"
+    # bin.install "bin/dotfiles"
+    # bin.install "bin/imgcat"
+    # bin.install "bin/iterm2-recv-zmodem"
+    # bin.install "bin/iterm2-send-zmodem"
+    # bin.install "bin/launchpad"
+    # bin.install "bin/trial"
+  end
+
+  test do
+    system "false"
+  end
+end

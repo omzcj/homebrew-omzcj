b9a0bf7350851839498b1c01ee2eb30622e97725
朝杰 张 <zhangchaojie@bytedance.com>
2021-05-24 00:22:15 +0800

    commit

--- Formula/omzcj.rb
+++ Formula/omzcj.rb
@@ -4,7 +4,9 @@
 class Omzcj < Formula
   desc "omzcj"
   homepage "https://assassinor.github.io"
-  url "https://codeload.github.com/assassinor/opt.git"
+  url "https://raw.githubusercontent.com/assassinor/opt/master/bin/app2ipa"
+  version "0.0.1"
+  sha256 "802ba738dc816ec870fadaa5df095e79b933fab32376fe9edb5f799342bb9d24"
   license ""
 
   # depends_on "cmake" => :build

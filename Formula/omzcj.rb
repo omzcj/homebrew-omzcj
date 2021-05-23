69b83671c8c9d0a930bd6fb24ffcca7ea730903e
朝杰 张 <7605023+assassinor@users.noreply.github.com>
2021-05-24 00:29:52 +0800

    commit

--- Formula/omzcj.rb
+++ Formula/omzcj.rb
@@ -17,7 +17,7 @@ class Omzcj < Formula
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    # bin.install "bin/*"
+    bin.install "bin/*"
   end
 
   test do

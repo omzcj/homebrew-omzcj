8e29c58e9d6369ec6a24889ec541b667140e7dd9
朝杰 张 <7605023+assassinor@users.noreply.github.com>
2021-05-23 21:27:37 +0800

    commit

--- Formula/trial-reset.rb
+++ Formula/trial-reset.rb
@@ -5,7 +5,7 @@ class TrialReset < Formula
   desc "trial reset"
   homepage "https://assassinor.github.io"
   url "https://raw.githubusercontent.com/assassinor/opt/master/trial-reset/bin/trial-reset"
-  version "0.0.2"
+  version "0.0.3"
   sha256 "927f270e494caf4217793dcd334c942140ad77b5350d77ada789c4271af59774"
   license ""
 

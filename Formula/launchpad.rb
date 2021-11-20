eea00fef33c1d04058bc1df57c2f787e93a0ec28
张朝杰 <zhangchaojie@bytedance.com>
2021-11-20 17:03:42 +0800

    commit

---
+++ Formula/launchpad.rb
@@ -0,0 +1,16 @@
+class Launchpad < Formula
+  desc "launchpad"
+  homepage "https://assassinor.github.io"
+  url "https://github.com/assassinor/omzcj.git"
+  version "0.0.1"
+  license ""
+
+  def install
+    system "make", "launchpad"
+    bin.install Dir["bin/*"]
+  end
+
+  test do
+    system "false"
+  end
+end

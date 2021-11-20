b8fa1371121c8f7d95d0a6bd00748f3b8be5e1c7
张朝杰 <7605023+assassinor@users.noreply.github.com>
2021-11-20 16:10:54 +0800

    commit

---
+++ iterm2-zmodem.rb
@@ -0,0 +1,16 @@
+class Iterm2Zmodem < Formula
+  desc "iterm2-zmodem"
+  homepage "https://assassinor.github.io"
+  url "https://github.com/assassinor/omzcj.git"
+  version "0.0.1"
+  license ""
+
+  def install
+    system "make", "build", "iterm2-zmodem"
+    bin.install Dir["bin/*"]
+  end
+
+  test do
+    system "false"
+  end
+end

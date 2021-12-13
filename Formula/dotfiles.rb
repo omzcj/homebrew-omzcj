bc5545817ed44350561ccc1be298a2603326177a
张朝杰 <zhangchaojie@bytedance.com>
2021-12-13 22:17:44 +0800

    commit

--- Formula/dotfiles.rb
+++ Formula/dotfiles.rb
@@ -1,7 +1,7 @@
 class Dotfiles < Formula
   desc "dotfiles"
   homepage "https://omzcj.github.io"
-  url "https://github.com/omzcj/dotfiles.git"
+  url "https://github.com/omzcj/dotfiles.git", branch: "main"
   version "0.0.1"
   license ""
 

99a76f726dc4aedf6a7639e5bbdde5270c3e131c
张朝杰 <7605023+assassinor@users.noreply.github.com>
2021-12-18 14:41:37 +0800

    commit

---
+++ Casks/niz-plum-keyboard.rb
@@ -0,0 +1,9 @@
+cask "niz-plum-keyboard" do
+  desc "niz plum 键盘校验工具"
+  homepage "https://assassinor.github.io"
+  version "1.0.2"
+  url "http://zhangchaojie.net/niz-plum-keyboard.dmg"
+  sha256 "c0614de6ef28d7b5dc10cf55df32946ef47663318cc319aa512b702462848853"
+  app "键盘校验工具v1.02.app"
+
+end

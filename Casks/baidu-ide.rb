82a56670183852c6d48c108beb58c0c0faa623cb
张朝杰 <zhangchaojie@bytedance.com>
2021-12-19 00:38:23 +0800

    ide

--- Casks/baidu-ide.rb
+++ Casks/baidu-ide.rb
@@ -3,7 +3,6 @@ cask "baidu-ide" do
   homepage "https://smartprogram.baidu.com"
   version "3.46.1"
   url "https://smartprogram.baidu.com/mappconsole/api/devDownload?system=mac&type=online"
-  sha256 "f604be611dd53044f72e87ae3d72bc46249655a6b7fa883643ac6f8d113abc07"
   app "百度开发者工具.app"
   name "百度开发者工具"
 end

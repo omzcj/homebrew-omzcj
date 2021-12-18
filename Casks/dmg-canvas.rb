922c85190feced008787f7985f9d9bc239e7e6e8
张朝杰 <7605023+assassinor@users.noreply.github.com>
2021-12-19 00:35:03 +0800

    ide

--- Casks/dmg-canvas.rb
+++ Casks/dmg-canvas.rb
@@ -1,6 +1,6 @@
 cask "dmg-canvas" do
   desc "DMG Canvas"
-  homepage "https://omzcj.github.io"
+  homepage "https://www.araelium.com/dmgcanvas/"
   version "3.0.15"
   url "https://github.com/omzcj/dmg-canvas/releases/download/3.0.15/DMG.Canvas.3.0.15.dmg"
   sha256 "bd07d890bc33735d9b992530b865914f2f0f24e132c3e205c03f903fc2987a96"

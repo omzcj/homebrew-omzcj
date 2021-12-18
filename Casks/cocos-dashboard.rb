64705edc277fb3d5673136186054ab3e33f9ff2e
张朝杰 <7605023+assassinor@users.noreply.github.com>
2021-12-18 23:49:39 +0800

    cocos-dashboard

---
+++ Casks/cocos-dashboard.rb
@@ -0,0 +1,9 @@
+cask "cocos-dashboard" do
+  desc "CocosDashboard"
+  homepage "https://cocos.com"
+  version "1.0.20"
+  url "https://download.cocos.com/CocosDashboard/v1.0.20/CocosDashboard-v1.0.20-mac-101211.dmg"
+  sha256 "af1fafd94f2920124e3d9e6f67d495938cbc705663ff982d9f7347fe84441127"
+  app "CocosDashboard.app"
+  name "CocosDashboard"
+end

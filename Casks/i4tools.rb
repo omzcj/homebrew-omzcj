c9d50c3dcdc167f3d04c16cdfc2621c417711983
张朝杰 <zhangchaojie@bytedance.com>
2021-12-18 20:08:36 +0800

    commit

---
+++ Casks/i4tools.rb
@@ -0,0 +1,11 @@
+cask "i4tools" do
+  desc "爱思助手"
+  homepage "https://www.i4.cn"
+  version "1.12.005"
+  url "https://url.i4.cn/fqIBBbaa"
+  sha256 "f81496966e06ac0870f4e77c2ab26e7225099c007bdfa90a01b1e5ae1533f9ee"
+  
+  pkg "i4tools.pkg"
+  name "i4tools"
+
+end

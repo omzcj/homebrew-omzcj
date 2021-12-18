35d2536c2d12caa8c8e48a8bc5bd348b543af769
张朝杰 <7605023+assassinor@users.noreply.github.com>
2021-12-19 02:01:04 +0800

    x

---
+++ Casks/x-paw.rb
@@ -0,0 +1,27 @@
+cask "paw" do
+  version "3.3.0"
+  sha256 "cc410fe95aeea3407d9ff242152ba2da5dd16f0e544629ae990b48799ea85363"
+
+  url "https://github.com/omzcj/x-paw/releases/download/#{version}/Paw.#{version}.dmg"
+  name "Paw"
+  desc "HTTP client that helps testing and describing APIs"
+  homepage "https://paw.cloud/"
+
+  livecheck do
+    url "https://paw.cloud/api/v2/updates/appcast"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sierra"
+
+  app "Paw.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.luckymarmot.Paw",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.luckymarmot.paw.sfl*",
+    "~/Library/Containers/com.luckymarmot.Paw",
+    "~/Library/Preferences/com.luckymarmot.Paw.plist",
+    "~/Library/Saved Application State/com.luckymarmot.Paw.savedState",
+  ]
+end

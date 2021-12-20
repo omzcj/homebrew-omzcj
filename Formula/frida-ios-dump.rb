d70b7e6b85daac82d4e5227a67e9dc301fb5eafd
张朝杰 <zhangchaojie@bytedance.com>
2021-12-20 15:35:43 +0800

    frida-ios-dump

---
+++ Formula/frida-ios-dump.rb
@@ -0,0 +1,15 @@
+class FridaIosDump < Formula
+  desc "frida-ios-dump"
+  homepage "https://omzcj.github.io/"
+  url "https://github.com/omzcj/frida-ios-dump.rb.git", branch: "main"
+  version "0.0.1"
+
+  def install
+    system "make", "build"
+    bin.install Dir["bin/*"]
+  end
+
+  test do
+    system "false"
+  end
+end

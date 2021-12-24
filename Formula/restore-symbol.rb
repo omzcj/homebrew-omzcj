8e8730f64f8ec7f754c0cb08d30e7c42a9eca8d1
张朝杰 <zhangchaojie@bytedance.com>
2021-12-24 16:47:38 +0800

    restore-symbol

--- Formula/restore-symbol.rb
+++ Formula/restore-symbol.rb
@@ -2,7 +2,7 @@ class RestoreSymbol < Formula
   desc "restore-symbol"
   homepage "https://github.com/tobefuturer/restore-symbol/"
   url "https://github.com/omzcj/restore-symbol/releases/download/1.0.0/restore-symbol"
-  sha265 "44016752de5e3b917707d35fcc39369bc539cbb524fdb8219aba5f15ed7292fa"
+  sha256 "44016752de5e3b917707d35fcc39369bc539cbb524fdb8219aba5f15ed7292fa"
   version "1.0.0"
 
   def install

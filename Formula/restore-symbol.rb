e45da71eaba9084c2d49df8336e6273186d193c0
张朝杰 <7605023+assassinor@users.noreply.github.com>
2021-12-24 16:46:47 +0800

    class-dump

--- Formula/restore-symbol.rb
+++ Formula/restore-symbol.rb
@@ -1,11 +1,11 @@
 class RestoreSymbol < Formula
   desc "restore-symbol"
   homepage "https://github.com/tobefuturer/restore-symbol/"
-  url "https://github.com/omzcj/restore-symbol.git", branch: "master"
+  url "https://github.com/omzcj/restore-symbol/releases/download/1.0.0/restore-symbol"
+  sha265 "44016752de5e3b917707d35fcc39369bc539cbb524fdb8219aba5f15ed7292fa"
   version "1.0.0"
 
   def install
-    system "make", "restore-symbol"
     bin.install Dir["restore-symbol"]
   end
 

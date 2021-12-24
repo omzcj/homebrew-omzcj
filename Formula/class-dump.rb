ba0edd64f7a1106ca888171631b3f811d9ad237f
张朝杰 <zhangchaojie@bytedance.com>
2021-12-24 15:57:41 +0800

    fork class-dump

--- Formula/class-dump.rb
+++ Formula/class-dump.rb
@@ -1,8 +1,7 @@
 class ClassDump < Formula
   desc "class-dump"
   homepage "http://stevenygard.com/"
-  url "https://raw.githubusercontent.com/AloneMonkey/MonkeyDev/master/bin/class-dump"
-  sha256 "2b6e88826f053792e496245a39259071ca4dd7accbd3feb6e60d37a245437514"
+  url "https://github.com/omzcj/class-dump.git", branch: "master"
   version "3.5"
 
   def install

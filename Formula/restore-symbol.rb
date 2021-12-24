bc3fc4a3bdb58ba3a6c6614c7118881fa4dc0bcb
张朝杰 <zhangchaojie@bytedance.com>
2021-12-24 16:31:41 +0800

    restore-symbol

--- Formula/restore-symbol.rb
+++ Formula/restore-symbol.rb
@@ -1,11 +1,11 @@
 class RestoreSymbol < Formula
   desc "restore-symbol"
   homepage "https://github.com/tobefuturer/restore-symbol/"
-  url "https://github.com/omzcj/restore-symbol", branch: "master"
+  url "https://github.com/omzcj/restore-symbol.git", branch: "master"
   version "1.0.0"
 
   def install
-    system "make"
+    system "make", "restore-symbol"
     bin.install Dir["restore-symbol"]
   end
 

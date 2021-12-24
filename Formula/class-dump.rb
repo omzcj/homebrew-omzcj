5afe32002e250d7f8487c131b69c85bace26344c
张朝杰 <7605023+assassinor@users.noreply.github.com>
2021-12-24 16:48:15 +0800

    class-dump

--- Formula/class-dump.rb
+++ Formula/class-dump.rb
@@ -2,7 +2,7 @@ class ClassDump < Formula
   desc "class-dump"
   homepage "http://stevenygard.com/"
   url "https://github.com/omzcj/class-dump/releases/download/3.6.0-alpha/class-dump"
-  sha265 "4b772e8f2482626800b745821cc9a631479d550b63d8023002a97381819d73da"
+  sha256 "4b772e8f2482626800b745821cc9a631479d550b63d8023002a97381819d73da"
   version "3.6.0-alpha"
 
   def install

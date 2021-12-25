b0270d81de4dcd363098d465cf0107edd17816d8
张朝杰 <zhangchaojie@bytedance.com>
2021-12-26 01:20:26 +0800

    optool

--- Formula/class-dump.rb
+++ Formula/class-dump.rb
@@ -1,9 +1,9 @@
 class ClassDump < Formula
   desc "class-dump"
+  version "3.6.0-alpha"
   homepage "http://stevenygard.com/"
   url "https://github.com/omzcj/class-dump/releases/download/#{version}/class-dump"
   sha256 "4b772e8f2482626800b745821cc9a631479d550b63d8023002a97381819d73da"
-  version "3.6.0-alpha"
 
   def install
     bin.install Dir["class-dump"]
--- Formula/optool.rb
+++ Formula/optool.rb
@@ -1,9 +1,9 @@
 class Optool < Formula
   desc "optool"
+  version "0.1"
   homepage "https://github.com/alexzielenski/optool/"
   url "https://github.com/alexzielenski/optool/releases/download/#{version}/optool.zip"
   sha256 "646effdfc6825eac47bf642609a4fbebe0f8d8abd494c3de99747e27c177a9ac"
-  version "0.1"
 
   def install
     bin.install Dir["optool"]

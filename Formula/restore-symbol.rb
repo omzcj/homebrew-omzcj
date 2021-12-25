f24185ddab7d4e07646fb389e1a90b4c3971ec6a
张朝杰 <7605023+assassinor@users.noreply.github.com>
2021-12-26 01:20:26 +0800

    optool

--- Formula/restore-symbol.rb
+++ Formula/restore-symbol.rb
@@ -1,9 +1,9 @@
 class RestoreSymbol < Formula
   desc "restore-symbol"
+  version "1.0.0"
   homepage "https://github.com/tobefuturer/restore-symbol/"
   url "https://github.com/omzcj/restore-symbol/releases/download/#{version}/restore-symbol"
   sha256 "44016752de5e3b917707d35fcc39369bc539cbb524fdb8219aba5f15ed7292fa"
-  version "1.0.0"
 
   def install
     bin.install Dir["restore-symbol"]

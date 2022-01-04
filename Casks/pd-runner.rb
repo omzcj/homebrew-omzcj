d7a50481a860e8643b40fd29c3e357470dfa2d36
张朝杰 <7605023+assassinor@users.noreply.github.com>
2022-01-04 14:29:39 +0800

    commit

--- Casks/pd-runner.rb
+++
@@ -1,9 +0,0 @@
-cask "pd-runner" do
-  desc "PD Runner"
-  homepage "https://github.com/lihaoyun6/PD-Runner/"
-  version "0.3.5"
-  url "https://github.com/lihaoyun6/PD-Runner/releases/download/0.3.5/PD-Runner_v0.3.5_Universal2.dmg"
-  sha256 "c0b96c361891c6d40854da77a05cb412a7e3e22cc349f7fef5abd35534fa0150"
-  app "PD Runner.app"
-  name "PD Runner"
-end

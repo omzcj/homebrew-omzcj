5c2af500647ba143c168e862b6114a9d07ddc1de
张朝杰 <zhangchaojie@bytedance.com>
2022-01-12 15:09:52 +0800

    lockscreen

---
+++ Formula/lockscreen.rb
@@ -0,0 +1,22 @@
+class Lockscreen < Formula
+  desc "lockscreen"
+  homepage "https://omzcj.github.io/"
+  url "https://github.com/omzcj/lockscreen.git", branch: "main"
+  version "0.0.1"
+
+  depends_on "etcd"
+
+  def install
+    system "make", "build"
+    bin.install Dir["bin/*"]
+  end
+
+  service do
+    run [opt_bin/"lockscreen"]
+    keep_alive true
+  end
+
+  test do
+    system "false"
+  end
+end

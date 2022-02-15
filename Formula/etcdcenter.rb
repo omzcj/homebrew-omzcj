4a9ab75525f8fe905472507dd4684fe6100563c9
张朝杰 <7605023+assassinor@users.noreply.github.com>
2022-02-15 22:53:41 +0800

    etcdcenter

---
+++ Formula/etcdcenter.rb
@@ -0,0 +1,20 @@
+class Etcdcenter < Formula
+  desc "etcdcenter"
+  homepage "https://omzcj.github.io/"
+  url "https://github.com/omzcj/etcdcenter.git", branch: "main"
+  version "0.0.1"
+
+  def install
+    system "make", "build"
+    bin.install Dir["bin/*"]
+  end
+
+  service do
+    run [opt_bin/"etcdcenter"]
+    keep_alive true
+  end
+
+  test do
+    system "false"
+  end
+end

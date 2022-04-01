d60c365d9ca47077756e39759a098946db4fabf7
张朝杰 <7605023+assassinor@users.noreply.github.com>
2022-04-01 19:47:06 +0800

    i4tools升级

--- Casks/i4tools.rb
+++ Casks/i4tools.rb
@@ -2,7 +2,7 @@ cask "i4tools" do
   desc "爱思助手"
   homepage "https://www.i4.cn/"
   version "1.12.016"
-  url "https://d.updater.i4.cn/i4mac/20220304/0/i4Tools_v1.12.016.dmg"
+  url "http://d.updater.i4.cn/i4mac/20220304/0/i4Tools_v1.12.016.dmg"
   sha256 "cb914e5dfe30a01c709a4b0bc9d32121548ec4593f2b0b2ae21e899f90c55df2"
   
   pkg "i4tools.pkg"

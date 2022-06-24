c6f5b3b0573f90951a80e2f136faf459e52d8c73
张朝杰 <7605023+assassinor@users.noreply.github.com>
2022-06-24 14:54:36 +0800

    update

--- Casks/baidu-ide.rb
+++ Casks/baidu-ide.rb
@@ -1,7 +1,7 @@
 cask "baidu-ide" do
   desc "百度开发者工具"
   homepage "https://smartprogram.baidu.com/"
-  version "3.46.1"
+  version "3.57.1"
   url "https://smartprogram.baidu.com/mappconsole/api/devDownload?system=mac&type=online"
   app "百度开发者工具.app"
   name "百度开发者工具"
--- Casks/i4tools.rb
+++ Casks/i4tools.rb
@@ -1,10 +1,8 @@
 cask "i4tools" do
   desc "爱思助手"
   homepage "https://www.i4.cn/"
-  version "1.12.016"
-  url "http://d.updater.i4.cn/i4mac/20220304/0/i4Tools_v1.12.016.dmg"
-  sha256 "00fe0fb84fde245d1de12b13962acf15fe8f4e35da7a76a70cec20eab95a8b1d"
-  
+  version "1.13"
+  url "https://d-updater.i4.cn/i4mac/20220418/0/i4Tools_v1.13.010.dmg"
   pkg "i4tools.pkg"
   name "i4tools"
 

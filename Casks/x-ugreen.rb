29a06856bfb4a160eae959095006f39f6fb10ec8
张朝杰 <zhangchaojie@bytedance.com>
2022-06-24 15:42:03 +0800

    update

---
+++ Casks/x-ugreen.rb
@@ -0,0 +1,8 @@
+cask "x-ugreen" do
+  desc "绿联云"
+  homepage "https://nas.lulian.cn/"
+  version "3.4.1"
+  url "https://ugreen.oss-cn-shenzhen.aliyuncs.com/ugreen/mac/UGREEN_Nas_v3.4.0.4615.dmg"
+  app "绿联云.app"
+  name "绿联云"
+end

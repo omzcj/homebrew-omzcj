89f590bbb1e83a03093eb9c5b95b1e67ffc23862
张朝杰 <zhangchaojie@bytedance.com>
2022-06-24 15:05:11 +0800

    update

---
+++ Casks/x-niz-plum-keyboard.rb
@@ -0,0 +1,9 @@
+cask "x-niz-plum-keyboard" do
+  desc "Niz Plum键盘校验工具"
+  homepage "https://www.nizkeyboard.com/"
+  version "1.0.2"
+  url "https://github.com/omzcj/x-niz-plum-keyboard/releases/download/1.0.2/niz-plum-keyboard-v1.0.2.dmg"
+  sha256 "c0614de6ef28d7b5dc10cf55df32946ef47663318cc319aa512b702462848853"
+  app "键盘校验工具v1.02.app"
+  name "键盘校验工具"
+end

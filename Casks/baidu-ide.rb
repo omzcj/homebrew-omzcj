3d87aa93d26bfc499c3837cf97df892d2b071564
张朝杰 <zhangchaojie@bytedance.com>
2023-01-17 21:42:20 +0800

    删除部分应用

--- Casks/baidu-ide.rb
+++
@@ -1,8 +0,0 @@
-cask "baidu-ide" do
-  desc "百度开发者工具"
-  homepage "https://smartprogram.baidu.com/"
-  version "3.57.1"
-  url "https://smartprogram.baidu.com/mappconsole/api/devDownload?system=mac&type=online"
-  app "百度开发者工具.app"
-  name "百度开发者工具"
-end
--- Casks/bytedance-ide.rb
+++
@@ -1,8 +0,0 @@
-cask "bytedance-ide" do
-  desc "小程序开发者工具"
-  homepage "https://microapp.bytedance.com/"
-  version "3.3.1"
-  url "https://sf1-cdn-tos.douyinstatic.com/obj/microapp/frontend/ide/package/obj/developer/ide/8397757/darwin/%E5%AD%97%E8%8A%82%E8%B7%B3%E5%8A%A8%E5%BC%80%E5%8F%91%E8%80%85%E5%B7%A5%E5%85%B7-3.3.1.dmg"
-  app "字节跳动开发者工具.app"
-  name "小程序开发者工具"
-end

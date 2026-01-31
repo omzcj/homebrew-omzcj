e69f5ae83992c133c199a39c953bfff531172779
张朝杰 <7605023+assassinor@users.noreply.github.com>
2026-01-31 17:43:55 +0800

    移除hopper-disassembler

--- Casks/hopper-disassembler.rb
+++
@@ -1,8 +0,0 @@
-cask "hopper-disassembler" do
-  desc "Hopper Disassembler"
-  homepage "https://www.hopperapp.com/"
-  version "5.6.0"
-  url "https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-#{version}-demo.dmg"
-  app "Hopper Disassembler v4.app"
-  name "Hopper Disassembler"
-end

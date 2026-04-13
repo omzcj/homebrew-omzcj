e2f3dbb6e4598304c432559c8a1483770ab57872
张朝杰 <zhangchaojie@bytedance.com>
2026-04-13 11:36:04 +0800

    Pasty

---
+++ Casks/pasty.rb
@@ -0,0 +1,9 @@
+cask "pasty" do
+  desc "Pasty"
+  homepage "https://github.com/scubers/pasty-release"
+  version "0.9.11"
+  url "https://github.com/scubers/pasty-release/releases/download/v#{version}/Pasty-#{version}.dmg"
+  sha256 "9c011607fe0ef2d9d4d5bf710a6c514c428fde156fedd90e4c1aa7b90dd17356"
+  app "Pasty.app"
+  name "Pasty"
+end

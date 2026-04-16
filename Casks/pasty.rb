cafe07335e55d7368f3a900a5a48a9254c91fe41
张朝杰 <7605023+assassinor@users.noreply.github.com>
2026-04-16 11:09:43 +0800

    更新pasty

--- Casks/pasty.rb
+++ Casks/pasty.rb
@@ -1,9 +1,9 @@
 cask "pasty" do
   desc "Pasty"
   homepage "https://github.com/scubers/pasty-release"
-  version "0.9.11"
+  version "0.9.12"
   url "https://github.com/scubers/pasty-release/releases/download/v#{version}/Pasty-#{version}.dmg"
-  sha256 "9c011607fe0ef2d9d4d5bf710a6c514c428fde156fedd90e4c1aa7b90dd17356"
+  sha256 :no_check 
   app "Pasty.app"
   name "Pasty"
 end

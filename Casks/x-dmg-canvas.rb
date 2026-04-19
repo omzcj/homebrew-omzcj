ef76e5be3d8f90d0497224eeae51ac7f739719f0
张朝杰 <7605023+assassinor@users.noreply.github.com>
2026-04-19 22:42:19 +0800

    删除x-dmg-canvas

--- Casks/x-dmg-canvas.rb
+++
@@ -1,9 +0,0 @@
-cask "x-dmg-canvas" do
-  desc "DMG Canvas"
-  homepage "https://www.araelium.com/dmgcanvas/"
-  version "3.0.15"
-  url "https://github.com/omzcj/x-dmg-canvas/releases/download/#{version}/DMG.Canvas.#{version}.dmg"
-  sha256 "bd07d890bc33735d9b992530b865914f2f0f24e132c3e205c03f903fc2987a96"
-  app "DMG Canvas.app"
-  name "DMG Canvas"
-end

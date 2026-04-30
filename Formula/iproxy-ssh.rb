8280b2a3b16e07f50f1267d1d795c49049affe5b
张朝杰 <7605023+assassinor@users.noreply.github.com>
2026-04-30 15:34:05 +0800

    iproxy-ssh

---
+++ Formula/iproxy-ssh.rb
@@ -0,0 +1,28 @@
+class IproxySsh < Formula
+  desc "Run iproxy 2222:22 for SSH over USB"
+  homepage "https://omzcj.github.io/"
+  url "https://github.com/omzcj/homebrew-omzcj.git", branch: "main"
+  version "0.0.1"
+
+  depends_on "libimobiledevice"
+
+  def install
+    (bin/"iproxy-ssh").write <<~SH
+      #!/bin/sh
+      exec "#{Formula["libimobiledevice"].opt_bin}/iproxy" 2222 22
+    SH
+    chmod 0755, bin/"iproxy-ssh"
+  end
+
+  service do
+    run [opt_bin/"iproxy-ssh"]
+    keep_alive true
+    log_path var/"log/iproxy-ssh.log"
+    error_log_path var/"log/iproxy-ssh.log"
+  end
+
+  test do
+    assert_path_exists bin/"iproxy-ssh"
+    assert_match "iproxy\" 2222 22", (bin/"iproxy-ssh").read
+  end
+end

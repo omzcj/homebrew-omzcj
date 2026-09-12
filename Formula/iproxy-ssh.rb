eec911c4ea8fbf6107c2afa05f6cc47143db06f0
张朝杰 <7605023+assassinor@users.noreply.github.com>
2026-09-12 18:13:01 +0800

    Refresh archive checksum after tap repository rename

--- Formula/iproxy-ssh.rb
+++ Formula/iproxy-ssh.rb
@@ -3,7 +3,7 @@ class IproxySsh < Formula
   homepage "https://github.com/oh-my-brew/homebrew-tap"
   url "https://github.com/oh-my-brew/homebrew-tap/archive/e34484a58e93d5cdeebda59a292cb9c731d3e66c.tar.gz"
   version "2026.05.28"
-  sha256 "bbc1aa63b7d92780359a638a2e017aa07f66f23be0128318066941099892b58b"
+  sha256 "5e0046964c7e1185a1aa008e8fb9aaeecb23fb88676fdede17d1196bb0118253"
 
   depends_on "libusbmuxd"
 
